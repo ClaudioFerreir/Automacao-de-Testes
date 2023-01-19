451f9c31867eecc7f80ef66bae04947f7adb45fb
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-01-19 19:31:25 -0300

    first access site

--- CAPYBARATESTE/tests/features/support/env.rb
+++ CAPYBARATESTE/tests/features/support/env.rb
@@ -1,2 +1,9 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
+
+Capybara.configure do |config|
+  #selenium->padrao(firefox) selenium_chrome->chrome selenium_chrome_healess->chrome sem gpu
+  config.default_driver = :selenium_chrome 
+  config.app_host = 'http://localhost:3000'
+  config.default_max_wait_time = 5
+end
