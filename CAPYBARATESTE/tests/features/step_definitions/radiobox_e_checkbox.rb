887238031928214a4b44b96b6967e10ac039ad8b
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-01-20 15:24:38 -0300

    radiobox and checkbox lesson

--- CAPYBARATESTE/tests/features/step_definitions/radiobox_e_checkbox.rb
+++ CAPYBARATESTE/tests/features/step_definitions/radiobox_e_checkbox.rb
@@ -1,3 +1,11 @@
 Quando('eu marco um checkboc e um radiobox') do
-  
+  visit('/buscaelementos/radioecheckbox')
+  find('label[for="white"]').click
+  sleep(2)
+  check('purple', allow_label_click: true) #necessario a opcao pq a label nao está visivel (opcao de marcar)
+  sleep(2)
+  uncheck('purple', allow_label_click: true) #necessario a opcao pq a label nao está visivel (desmarcar)
+  sleep(5)
+  choose('red', allow_label_click: true) #label clicavel (opcao de marcar)
+  sleep(2)
 end
