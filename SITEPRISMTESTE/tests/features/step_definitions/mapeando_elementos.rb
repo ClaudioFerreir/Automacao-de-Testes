8a83532b0f9db029272deb7f31fed5d2473123a1
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-01-23 20:14:21 -0300

    instanciando melhor um objeto lesson

--- SITEPRISMTESTE/tests/features/step_definitions/mapeando_elementos.rb
+++ SITEPRISMTESTE/tests/features/step_definitions/mapeando_elementos.rb
@@ -1,9 +1,12 @@
 Quando('preencho o formulario.') do
-  @mapeando = MapeandoElementoPage.new
+  #@mapeando = MapeandoElementoPage.new
+  #@mapeando.load
+  #@mapeando.preencher #chamamos o metodo
 
-  @mapeando.load
+  #outra forma mais pratica para nao precisarmos instanciar todas as vezes ja que fizemos no page_helper e esta disponivel para todo o projeto. Assim so precisamos instaciar a classe uma vez e nao toda vez que formos usar.
+  home.load 
+  home.preencher
 
-  @mapeando.preencher #chamamos o metodo
   sleep(5)
   # @mapeando.nome.set 'claudio' #mesma coisa so que sem a praticidade do metodo
 end
--- SITEPRISMTESTE/tests/features/support/env.rb
+++ SITEPRISMTESTE/tests/features/support/env.rb
@@ -1,6 +1,9 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'site_prism'
+require_relative 'page_helper.rb' #carregamos o arquivo que criamos
+
+World(PageObjects) #colocamos o nosso modulo como global assim podemos utiliza-lo em todo o nosso projeto
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
